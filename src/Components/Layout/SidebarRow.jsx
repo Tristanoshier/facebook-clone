99d763413be1c8e652339a65e59b7b8be39a9f8e
Tristanoshier <tsoshier@bsu.edu>
2021-04-11 13:21:26 -0400

    set up sidebar

--- src/Components/Layout/SidebarRow.jsx
+++ src/Components/Layout/SidebarRow.jsx
@@ -1,7 +1,11 @@
-export const SidebarRow = () => {
+import { Avatar } from '@material-ui/core';
+
+export const SidebarRow = ({ src, Icon, title }) => {
     return (
-        <div>
-            
+        <div className='sidebarRow'>
+            {src && <Avatar src={src} />}
+            {Icon && <Icon />}
+            <h4>{title}</h4>
         </div>
     )
 }
