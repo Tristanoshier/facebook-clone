41cebaca8307faca1d4b1af5411fdc5afa02c4e9
Tristanoshier <tsoshier@bsu.edu>
2021-04-11 13:17:01 -0400

    add sample data for story feature

--- src/Components/Feed/Stories/Story.jsx
+++ src/Components/Feed/Stories/Story.jsx
@@ -1,8 +1,10 @@
+import { Avatar } from '@material-ui/core';
 
-export const Story = () => {
+export const Story = ({ image, profileSrc, title}) => {
     return (
-        <div>
-            
+        <div style={{backgroundImage: `url(${image})`}} className='story'>
+            <Avatar className='story-avatar' src={profileSrc} />
+            <h4>{title}</h4>
         </div>
     )
 }
--- src/Components/Feed/Stories/StoryReel.jsx
+++ src/Components/Feed/Stories/StoryReel.jsx
@@ -1,8 +1,26 @@
+import { Story } from './Story';
 
 export const StoryReel = () => {
     return (
-        <div>
-            
+        <div className='storyReel'>
+            <Story 
+                title='Tristan'
+                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+
+            />
+            <Story 
+                title='Hayden'
+                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+            />
+            <Story 
+                title='Izzy'
+                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+            />
+            <Story 
+                title='Kylie'
+                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+            />
+
         </div>
     )
 }
