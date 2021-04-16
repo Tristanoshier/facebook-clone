13775623bdbadb400e855eb59623f5ad8333162d
Tristanoshier <tsoshier@bsu.edu>
2021-04-16 11:25:38 -0400

    add favicons and better images for site

--- src/Components/Feed/Stories/StoryReel.jsx
+++ src/Components/Feed/Stories/StoryReel.jsx
@@ -4,21 +4,21 @@ export const StoryReel = () => {
     return (
         <div className='storyReel'>
             <Story 
-                title='Tristan'
-                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+                title='John'
+                image='https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2662&q=80'
 
             />
             <Story 
-                title='Hayden'
-                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+                title='Katie'
+                image='https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2459&q=80'
             />
             <Story 
-                title='Izzy'
-                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+                title='Lucas'
+                image='https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80'
             />
             <Story 
                 title='Kylie'
-                image='https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2020/05/Mark-Zuckerberg-e1590173646243-960x722.jpg'
+                image='https://images.unsplash.com/photo-1519699047748-de8e457a634e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA3fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
             />
 
         </div>
